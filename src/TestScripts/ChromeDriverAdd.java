d6a0fd53ba28a479fcdfb872b6639e4ceafa72fd
kgit1 <kgit1@rambler.ru>
2017-03-20 00:01:45 +0200

    up

--- src/TestScripts/ChromeDriverAdd.java
+++ src/TestScripts/ChromeDriverAdd.java
@@ -1,9 +1,10 @@
 package TestScripts;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class chromeDriver {
+public class ChromeDriverAdd {
 
 	public static void main(String[] args) {
 		
@@ -21,15 +22,21 @@ public class chromeDriver {
 		WebDriver driver = new ChromeDriver();
 		
 		//visit google.com(don't forget http:// at begining)
+		// void get(java.lang.String url)
+		// Load a new web page in the current browser window. This is done using
+		// an HTTP GET operation, and the method will block until the load is
+		// complete. This will follow redirects issued either by the server or
+		// as a meta-redirect from within the returned HTML. Should a
+		// meta-redirect "rest" for any duration of time, it is best to wait
+		// until this timeout is over, since should the underlying page change
+		// whilst your test is executing the results of future calls against
+		// this interface will be against the freshly loaded page. Synonym for
+		// WebDriver.Navigation.to(String).
+		// Parameters:
+		// url - The URL to load. It is best to use a fully qualified URL
 		driver.get("http://www.google.com");
 		
 		//maximize opened window
-		driver.manage().window().maximize();
-		
-		
-		
-		
-		
+		driver.manage().window().maximize();		
 	}
-
 }
--- src/TestScripts/EdgeDriverAdd.java
+++ src/TestScripts/EdgeDriverAdd.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-public class edgeDriver {
+public class EdgeDriverAdd {
 
 	public static void main(String[] args) {
 
