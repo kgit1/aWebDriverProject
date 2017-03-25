d0301b03af89d3fa60d26f1803bc8896724bf9b8
kgit1 <kgit1@rambler.ru>
2017-03-25 03:12:30 +0200

    up

--- src/testNG_1/LoginTest.java
+++ src/testNG_1/LoginTest.java
@@ -1,5 +1,8 @@
 package testNG_1;
 
+import static org.testng.Assert.assertEquals;
+
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTest {
@@ -7,11 +10,34 @@ public class LoginTest {
 	@Test
 	public void testLoginViaEmail() {
 		System.out.println("Login via Email");
+		// Assertion - utverzdenie
+		// Assert - Assertion tool class. Presents assertion methods with a more
+		// natural parameter order. The order is always actualValue,
+		// expectedValue
+		// assertEquals(actualValue, expectedValue) - asserts that two valuess
+		// are equal. If they are not, an AssertionError is thrown
+		// Assert.assertEquals("Vaibhav", "Vaibhav1");
+
+		// Asserts that a condition is true. If it isn't, an AssertionError is
+		// thrown
+		// Assert.assertTrue(1 > 4);
+		// Asserts that a condition is true. If it isn't, an AssertionError is
+		// thrown
+		Assert.assertFalse(1 > 4);
+
 	}
 
 	@Test
 	public void testLoginViaFacebook() {
 		System.out.println("Login via facebook");
+		// when we need to handle error - to catch and same time allow method to
+		// finish
+		try {
+			Assert.assertEquals("Vaibhav", "Vaibhav1");
+		} catch (Throwable t) {
+			System.out.println(t);
+		}
+		System.out.println("Afeter error - Login via facebook");
 	}
 
 	@Test
