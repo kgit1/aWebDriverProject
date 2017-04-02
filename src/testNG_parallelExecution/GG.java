b1eebc2bdb26fe5ae9e068b566130e3e7a3a09f5
kgit1 <kgit1@rambler.ru>
2017-04-02 20:12:12 +0300

    up

--- src/testNG_parallelExecution/GG.java
+++ src/testNG_parallelExecution/GG.java
@@ -15,50 +15,73 @@ import org.testng.annotations.Test;
 
 public class GG {
 
-	@Test
-	public void startGG() throws InterruptedException, AWTException {
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver.exe");
+	// @Test
+	// public void startGG() throws InterruptedException, AWTException {
+	// System.setProperty("webdriver.chrome.driver",
+	// System.getProperty("user.dir") + "/drivers/chromedriver.exe");
+	//
+	// for (int i = 0; i < 10; i++) {
+	// ChromeOptions options = new ChromeOptions();
+	// options.addArguments("--mute-audio");
+	//
+	// WebDriver driver = new ChromeDriver(options);
+	// driver.get("https://goodgame.ru/channel/ZERG/#autoplay");
+	// driver.manage().window().maximize();
+	// driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+	//
+	// Point coordinates =
+	// driver.findElement(By.xpath("//*[@id='_qualitySwitch']")).getLocation();
+	// System.out.println("Co-ordinates" + coordinates);
+	// Robot robot = new Robot();
+	// System.out.println("move 0");
+	// robot.mouseMove(coordinates.getX(), coordinates.getY());
+	// System.out.println("move 20");
+	// robot.mouseMove(coordinates.getX() + 20, coordinates.getY() + 10);
+	// // robot.mousePress( InputEvent.BUTTON1_DOWN_MASK);
+	// // robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+	// System.out.println("move 200");
+	// for (int j = 400; j < 800; j = j + 20) {
+	// System.out.println(j);
+	// Thread.sleep(20);
+	// robot.mouseMove(coordinates.getX() + 500 + j, coordinates.getY() + 20);
+	// }
+	//
+	// driver.findElement(By.xpath("//*[@id='_qualitySwitch']/div/div[1]")).click();
+	// driver.findElement(By.xpath("//*[@id='_qualitySwitch']/div/ul/li[5]/a")).click();
+	// }
+	// }
 
-		for (int i = 0; i < 10; i++) {
-			ChromeOptions options = new ChromeOptions();
-			// options.addArguments("--mute-audio");
-
-			WebDriver driver = new ChromeDriver(options);
-			driver.get("https://goodgame.ru/channel/ZERG/#autoplay");
-			driver.manage().window().maximize();
-			driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-
-			Point coordinates = driver.findElement(By.xpath("//*[@id='_qualitySwitch']")).getLocation();
-			System.out.println("Co-ordinates" + coordinates);
-			Robot robot = new Robot();
-			System.out.println("move 0");
-			robot.mouseMove(coordinates.getX(), coordinates.getY());
-			System.out.println("move 20");
-			robot.mouseMove(coordinates.getX() + 20, coordinates.getY() + 10);
-			// robot.mousePress( InputEvent.BUTTON1_DOWN_MASK);
-			// robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-			System.out.println("move 200");
-			for (int j = 400; j < 800; j = j + 20) {
-				System.out.println(j);
-				Thread.sleep(20);
-				robot.mouseMove(coordinates.getX() + 500 + j, coordinates.getY() + 20);
-			}
-
-			driver.findElement(By.xpath("//*[@id='_qualitySwitch']/div/div[1]")).click();
-			// *[@id="_qualitySwitch"]/div/ul/li[5]/a
-
-			System.out.println("move 300");
-			// robot.mouseMove(coordinates.getX() + 700, coordinates.getY() +
-			// 200);
-			driver.findElement(By.xpath("//*[@id='_qualitySwitch']/div/ul/li[5]/a")).click();
-		}
-
-		// Actions action = new Actions(driver);
-		// for (int i = 100; i < 500; i = i + 10) {
-		// System.out.println(i);
-		// Thread.sleep(1000);
-		// action.moveToElement(driver.findElement(By.xpath("//*[@id='entire']/div")),
-		// i, 400).click();
-		// }
-	}
+	// @Test
+	// public void startGG() throws InterruptedException, AWTException {
+	// System.setProperty("webdriver.chrome.driver",
+	// System.getProperty("user.dir") + "/drivers/chromedriver.exe");
+	//
+	// // for (int i = 0; i < 10; i++) {
+	// ChromeOptions options = new ChromeOptions();
+	// options.addArguments("--mute-audio");
+	//
+	// WebDriver driver = new ChromeDriver(options);
+	// driver.get("https://goodgame.ru/channel/ZERG/#autoplay");
+	// driver.manage().window().maximize();
+	// driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+	//
+	// Actions action = new Actions(driver);
+	// for (int j = 100; j < 500; j = j + 20) {
+	// action.moveToElement(
+	// driver.findElement(By.xpath("//*[@id='entire']/div/div[1]/div[2]/div[2]/span[2]/span/a"))).click()
+	// // Generates a composite action containing all actions so
+	// // far,
+	// // ready to be performed (and resets the internal builder
+	// // state,
+	// // so subsequent calls to build() will contain fresh
+	// // sequences).
+	// .build()
+	// // A convenience method for performing the actions without
+	// // calling build() first.(? not working without build first)
+	// .perform();
+	// }
+	// driver.findElement(By.xpath("//*[@id='_qualitySwitch']/div/div[1]")).click();
+	// System.out.println("move 300");
+	// driver.findElement(By.xpath("//*[@id='_qualitySwitch']/div/ul/li[5]/a")).click();
+	// }
 }
